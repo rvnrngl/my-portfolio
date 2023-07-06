b939860605f8fa33ceabbce677f1828525f0b1f0
Reevss <0217.raven.ringel@gmail.com>
2023-07-06 16:06:10 +0800

    edit: font-size to match small screen devices

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,13 +14,13 @@ const Cards = ({ name, description, src, srcPages, icons }) => {
           src={src}
           alt="fashion-step image"
         />
-        <div className="absolute -bottom-full left-8 group-hover:bottom-[30%] transition-all duration-300 z-50">
+        <div className="absolute -bottom-full left-4 lg:left-6 group-hover:bottom-[30%] transition-all duration-300 z-50">
           <span className="text-md md:text-2xl font-semibold">{name}</span>
         </div>
-        <div className="absolute -bottom-full left-8 group-hover:bottom-[20%] transition-all duration-500 z-50">
+        <div className="absolute -bottom-full left-4 lg:left-6 group-hover:bottom-[20%] transition-all duration-500 z-50">
           <p className="text-paragraph text-sm md:text-xl">{description}</p>
         </div>
-        <div className="absolute -bottom-full left-8 group-hover:bottom-[7%] transition-all duration-700 z-50">
+        <div className="absolute -bottom-full left-4 lg:left-6 group-hover:bottom-[7%] transition-all duration-700 z-50">
           <div className="w-[18px] h-[18px] lg:w-[28px] lg:h-[28px] flex gap-3 items-center opacity-80">
             {icons.map((items) => {
               return (
@@ -34,14 +34,14 @@ const Cards = ({ name, description, src, srcPages, icons }) => {
             })}
           </div>
         </div>
-        <div className="absolute -bottom-full right-8 group-hover:bottom-[7%] transition-all duration-700 z-50">
+        <div className="absolute -bottom-full right-4 lg:right-6 group-hover:bottom-[7%] transition-all duration-700 z-50">
           <a
             href={srcPages}
             target="_blank"
-            className="text-paragraph cursor-pointer hover:bg-paragraph underline underline-offset-[12px] hover:no-underline text-md font-khand font-bold rounded-sm p-2 
+            className="text-paragraph cursor-pointer hover:bg-paragraph underline underline-offset-[12px] hover:no-underline text-sm lg:text-md font-khand font-semibold rounded-sm p-2 
                           duration-200 ease-in hover:text-btn-text-color hover:shadow-[5px_5px_0px_#000000] hover:translate-x-[-2px]"
           >
-            View in GitHub Pages
+            View in GitHub
           </a>
         </div>
       </div>
